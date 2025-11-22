fe4969400e320482d9ac21728763eebd42286d06
Sohail Siadat <sohale@gmail.com>
2025-11-22 21:12:26 +0000

    Update replace_templates.bash

--- scripts/replace_templates.bash
+++ scripts/replace_templates.bash
@@ -3,10 +3,11 @@
 
 set -ex
 
-REPLC_TEXT=\"$(date) $(pwd)\"
+REPLC_TEXT="$(date) $(pwd)"
 printf "%s\n" "$REPLC_TEXT" > /tmp/replc.text
 printf "s|hand: @@|hand: __REPLC__|" > /tmp/sed_script.sed
 sed -i "s|__REPLC__|$(cat /tmp/replc.text)|" /tmp/sed_script.sed
 sed -i -f /tmp/sed_script.sed index.html
+
 # printf "%s\n" "$REPLC_TEXT" > /tmp/replc.text
 # sed -i \"s/hand: @@/${REPLC_TEXT}/\" index.html
